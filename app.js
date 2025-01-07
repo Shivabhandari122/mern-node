9fd2c99160331e704947f30e1a3cff8ad80854f4
Shivabhandari122 <qshiva906@gmail.com>
2025-01-07 23:21:39 +0545

    backend  learning

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./database')
 const app = express()
 
+app.use(express.json)
+
 connectToDatabase()
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("nodejs project has been started")
 })
 
@@ -13,4 +16,11 @@ app.get('/about', (req, res) =>{
     res.json({
         message: "This is home page"
      })
+})
+
+app.post('/blog', (req, res) =>{
+    console.log(req.body)
+    res.status(200).json({
+        message: "blog api hit successfully"
+    })
 })
--- database/index.js
+++ database/index.js
@@ -1,8 +1,8 @@
 const mon = require('mongoose')
 
 async function connectToDatabase(){
-    await mon.connect('mongodb+srv://qshiva906:whitehouse@cluster0.6ftcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-    console.log('connected to the database sucessfully')
+    await mon.connect(process.env.MONGODB_URL)
+    console.log('database connected sucessfully')
 }
 
 module.exports = connectToDatabase
